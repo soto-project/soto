3a9785a9860c586a9d315eccf4e02b1ed051ff4f
noppoman <yuki@miketokyo.com>
2017-04-27 12:58:40 -0700

    implement Pure JSONSerializer to avoid 'The operation could not be completed' with JSONSerialization.data()

--- Sources/Core/Serializer/JSONSerializer.swift
+++ Sources/Core/Serializer/JSONSerializer.swift
@@ -7,28 +7,85 @@
 //
 
 import Foundation
-import SwiftyJSON
+
+private func dquote(_ str: String) -> String {
+    return "\"\(str)\""
+}
+
+protocol NumericType {
+    static func +(lhs: Self, rhs: Self) -> Self
+    static func -(lhs: Self, rhs: Self) -> Self
+    static func *(lhs: Self, rhs: Self) -> Self
+    static func /(lhs: Self, rhs: Self) -> Self
+    static func %(lhs: Self, rhs: Self) -> Self
+    init(_ v: Int)
+}
+
+extension Double : NumericType { }
+extension Float  : NumericType { }
+extension Int    : NumericType { }
+extension Int8   : NumericType { }
+extension Int16  : NumericType { }
+extension Int32  : NumericType { }
+extension Int64  : NumericType { }
+extension UInt   : NumericType { }
+extension UInt8  : NumericType { }
+extension UInt16 : NumericType { }
+extension UInt32 : NumericType { }
+extension UInt64 : NumericType { }
+
+
+enum JSONSerializerError: Error {
+    case unserializableType(String)
+}
+
+private func serialize(value: Any) throws -> String {
+    var s = ""
+    switch value {
+    case let dict as [String: Any]:
+        s += try "{" + serialize(dictionary: dict) + "}"
+        
+    case let elements as [Any]:
+        s += try serialize(array: elements)
+        
+    case let v as NumericType:
+        s += "\(v)"
+        
+    case let v as Bool:
+        s += "\(v)".lowercased()
+        
+    case _ as Data:
+        throw JSONSerializerError.unserializableType("Data")
+        
+    default:
+        s += dquote("\(value)")
+    }
+    
+    return s
+}
+
+private func serialize(array: [Any]) throws -> String {
+    var s = ""
+    for (index, item) in array.enumerated() {
+        s += try serialize(value: item)
+        if array.count - index > 1 { s += ", " }
+    }
+    return "[" + s + "]"
+}
+
+private func serialize(dictionary: [String: Any]) throws -> String {
+    var s = ""
+    for (offset: index, element: (key: key, value: value)) in dictionary.enumerated() {
+        s += dquote(key)+": "
+        s += try serialize(value: value)
+        if dictionary.count - index > 1 { s += ", " }
+    }
+    return s
+}
 
 struct JSONSerializer {
     public func serialize(_ dictionary: [String: Any]) throws -> Data {
-        var dictionary = dictionary
-        // TODO Should recursive check.
-        for (key, value) in dictionary {
-            switch value {
-            case let v as Date:
-                dictionary[key] = "\(v)"
-                
-            default:
-                break
-            }
-        }
-        
-        let data = try JSONSerialization.data(withJSONObject: dictionary, options: JSONSerialization.WritingOptions(rawValue: 0))
-        
-        guard let json = String(data: data, encoding: .utf8)?.replacingOccurrences(of: "\\", with: "") else {
-            return Data()
-        }
-        
-        return json.data(using: .utf8) ?? Data()
+        let jsonString = try "{" + Core.serialize(dictionary: dictionary) + "}"
+        return jsonString.data(using: .utf8) ?? Data()
     }
 }
