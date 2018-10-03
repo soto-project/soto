82961e77cfd241d3c4f6bf7c5b9628befda8610b
Jonathan McAllister <jonathan@blast.com>
2018-10-03 14:12:30 -0700

    updates to generate targets to support new aws naming convention

--- scripts/generate_targets.swift
+++ scripts/generate_targets.swift
@@ -8,8 +8,8 @@ enum GenerationError: Error {
 
 let manager = FileManager.default
 
-let servicesBasePath = "../Sources/AWSSDKSwift/Services"
-let middlewaresBasePath = "../Sources/AWSSDKSwift/Middlewares"
+let servicesBasePath = "./Sources/AWSSDKSwift/Services"
+let middlewaresBasePath = "./Sources/AWSSDKSwift/Middlewares"
 
 func findDirectoryNames(at path: String) -> [String]? {
     guard let names = try? manager.contentsOfDirectory(atPath: path) else {
@@ -29,18 +29,18 @@ guard let middlewares = findDirectoryNames(at: middlewaresBasePath) else {
 
 let modules = services + middlewares.map { "\($0)Middleware" }
 
-let sdkDependencies = modules.map { "\"\($0.capitalized)\"," }.sorted().joined(separator: "")
+let sdkDependencies = modules.map { "\"\($0)\"," }.sorted().joined(separator: "")
 print("Dependencies for .library(AWSSDKSwift):")
-print("\(sdkDependencies)]),")
+print("([\(sdkDependencies)]),")
 
-let libraries =  services.map( { ".library(name: \"\($0.capitalized)\", targets: [\"\($0.capitalized)\"]),\n" }).sorted().joined(separator: "")
+let libraries =  services.map( { ".library(name: \"\($0)\", targets: [\"\($0)\"]),\n" }).sorted().joined(separator: "")
 print("Full list of libraries:")
 print(libraries)
 
 
 //TODO(Yasumoto): Need to auto-add S3RequestMiddleware to S3 module dependency
-let servicesTargetDefinitions = services.map { "        .target(name: \"\($0.capitalized)\", dependencies: [\"AWSSDKSwiftCore\"], path: \"\(servicesBasePath)/\($0)\")," }
+let servicesTargetDefinitions = services.map { "        .target(name: \"\($0)\", dependencies: [\"AWSSDKSwiftCore\"], path: \"\(servicesBasePath)/\($0)\")," }
 _ = servicesTargetDefinitions.sorted().map { print($0) }
 
-let middlewaresTargetDefinitions = middlewares.map { "        .target(name: \"\($0.capitalized)Middleware\", dependencies: [\"AWSSDKSwiftCore\"], path: \"\(middlewaresBasePath)/\($0)\")," }
+let middlewaresTargetDefinitions = middlewares.map { "        .target(name: \"\($0)Middleware\", dependencies: [\"AWSSDKSwiftCore\"], path: \"\(middlewaresBasePath)/\($0)\")," }
 _ = middlewaresTargetDefinitions.sorted().map { print($0) }
