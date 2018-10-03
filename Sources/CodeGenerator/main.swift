a1a0298151e86b4f59c470d7f8a01e1609c753b8
Jonathan McAllister <jonathan@blast.com>
2018-10-03 13:14:28 -0700

    update code generator to follow aws naming convention for modules #78 #86

--- Sources/CodeGenerator/main.swift
+++ Sources/CodeGenerator/main.swift
@@ -15,31 +15,31 @@ var errorShapeMap: [String: String] = [:]
 for index in 0..<apis.count {
     let api = apis[index]
     let doc = docs[index]
-    let serviceName = api["metadata"]["endpointPrefix"].stringValue.toSwiftClassCase()
-    
+
     group.enter()
     DispatchQueue.global().async {
         do {
-            log("Generating \(serviceName) codes ........")
-            
             let service = try AWSService(fromAPIJSON: api, docJSON: doc, endpointJSON: endpoint)
-            let basePath = "\(rootPath())/Sources/AWSSDKSwift/Services/\(api["serviceName"].stringValue)/"
+
+            log("Generating \(service.serviceName) codes ........")
+
+            let basePath = "\(rootPath())/Sources/AWSSDKSwift/Services/\(service.serviceName)/"
             _ = mkdirp(basePath)
-            
+
             try service.generateServiceCode()
                 .write(
                     toFile: "\(basePath)/\(service.serviceName)_API.swift",
                     atomically: true,
                     encoding: .utf8
                 )
-            
+
             try service.generateShapesCode()
                 .write(
                     toFile: "\(basePath)/\(service.serviceName)_Shapes.swift",
                     atomically: true,
                     encoding: .utf8
                 )
-            
+
             if !service.errorShapeNames.isEmpty {
                 errorShapeMap[service.endpointPrefix] = service.serviceErrorName
                 try service.generateErrorCode()
@@ -49,8 +49,8 @@ for index in 0..<apis.count {
                         encoding: .utf8
                 )
             }
-            
-            log("Succesfully Generated \(serviceName) codes!")
+
+            log("Succesfully Generated \(service.serviceName) codes!")
             group.leave()
         } catch {
             DispatchQueue.main.sync {
