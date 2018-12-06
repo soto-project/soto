b66a8ad7f949ef80a503069603ad1abd240e7f7d
Jonathan McAllister <jonathan@blast.com>
2018-12-05 16:37:42 -0800

    update Readme to v2.0.2

--- scripts/gen-doc.sh
+++ scripts/gen-doc.sh
@@ -1,10 +1,10 @@
-for d in ../Sources/AWSSDKSwift/Services/*;
+for d in Sources/AWSSDKSwift/Services/*;
   do moduleName="$(basename "$d")";
   sourcekitten doc --spm-module $moduleName > "$moduleName".json;
 done;
 jq -s '[.[][]]' *.json > awssdkswift.json;
 ls | grep "^[A-Z].*.json" | xargs rm -f
-jazzy --theme ../jazzy/themes/apple-thin-nav/
+jazzy --theme jazzy/themes/apple-thin-nav/
 # use theme apple-thin-nav else docs are 50+ GB!
 rm -rf docs/docsets
 # then
