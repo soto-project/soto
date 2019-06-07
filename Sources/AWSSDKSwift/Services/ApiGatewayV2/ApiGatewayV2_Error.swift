1fd0b51d75854d4505a05cc4cfd011b75ffdf702
Adam Fowler <adamfowler71@gmail.com>
2019-06-07 10:04:03 +0100

    Rebuilt service files to fix the few firstIndex() warnings that appear after a merge

--- Sources/AWSSDKSwift/Services/ApiGatewayV2/ApiGatewayV2_Error.swift
+++ Sources/AWSSDKSwift/Services/ApiGatewayV2/ApiGatewayV2_Error.swift
@@ -13,7 +13,7 @@ public enum ApiGatewayV2ErrorType: AWSErrorType {
 extension ApiGatewayV2ErrorType {
     public init?(errorCode: String, message: String?){
         var errorCode = errorCode
-        if let index = errorCode.index(of: "#") {
+        if let index = errorCode.firstIndex(of: "#") {
             errorCode = String(errorCode[errorCode.index(index, offsetBy: 1)...])
         }
         switch errorCode {
--- Sources/AWSSDKSwift/Services/Textract/Textract_Error.swift
+++ Sources/AWSSDKSwift/Services/Textract/Textract_Error.swift
@@ -21,7 +21,7 @@ public enum TextractErrorType: AWSErrorType {
 extension TextractErrorType {
     public init?(errorCode: String, message: String?){
         var errorCode = errorCode
-        if let index = errorCode.index(of: "#") {
+        if let index = errorCode.firstIndex(of: "#") {
             errorCode = String(errorCode[errorCode.index(index, offsetBy: 1)...])
         }
         switch errorCode {
