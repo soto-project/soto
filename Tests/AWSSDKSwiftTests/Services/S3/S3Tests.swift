32354cc255956646f396536f3dd23b9d92c8ef42
Adam Fowler <adamfowler71@gmail.com>
2019-06-28 07:37:10 +0100

    Added test to check Request XML for createMultipartUpload

--- Tests/AWSSDKSwiftTests/Services/S3/S3Tests.swift
+++ Tests/AWSSDKSwiftTests/Services/S3/S3Tests.swift
@@ -86,12 +86,27 @@ class S3Tests: XCTestCase {
         XCTAssertEqual(output.contents?.first?.eTag, putResult.eTag?.replacingOccurrences(of: "\"", with: ""))
     }
 
-
+    func testCreateMultipartUploadXML() {
+        let request = S3.CreateMultipartUploadRequest(acl: .authenticatedRead, bucket: "test-bucket", expires: TimeStamp(Date(timeIntervalSince1970: 10000000)), key:"test-object", metadata: ["test-key":"test-value"], objectLockLegalHoldStatus:.on, objectLockMode: .compliance, objectLockRetainUntilDate: TimeStamp(Date(timeIntervalSince1970: 10003600)), requestPayer: .requester, serverSideEncryption: .aes256, storageClass: .standard)
+        do {
+            let expectedResult = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><CreateMultipartUploadRequest><x-amz-acl>authenticated-read</x-amz-acl><Bucket>test-bucket</Bucket><Expires>1970-04-26T17:46:40.000Z</Expires><Key>test-object</Key><Metadata><entry><key>test-key</key><value>test-value</value></entry></Metadata><x-amz-object-lock-legal-hold>ON</x-amz-object-lock-legal-hold><x-amz-object-lock-mode>COMPLIANCE</x-amz-object-lock-mode><x-amz-object-lock-retain-until-date>1970-04-26T18:46:40.000Z</x-amz-object-lock-retain-until-date><x-amz-request-payer>requester</x-amz-request-payer><x-amz-server-side-encryption>AES256</x-amz-server-side-encryption><x-amz-storage-class>STANDARD</x-amz-storage-class></CreateMultipartUploadRequest>"
+            let awsRequest = try client.client.debugCreateAWSRequest(operation: "CreateMultipartUpload", path: "/{Bucket}/{Key+}?uploads", httpMethod: "POST", input: request)
+            
+            let bodyData = try awsRequest.body.asData()
+            XCTAssertNotNil(bodyData)
+            let body = String(data:bodyData!, encoding: .utf8)!
+            XCTAssertEqual(body, expectedResult)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+    
     static var allTests : [(String, (S3Tests) -> () throws -> Void)] {
         return [
             ("testPutObject", testPutObject),
             ("testListObjects", testListObjects),
             ("testGetObject", testGetObject),
+            ("testCreateMultipartUploadXML", testCreateMultipartUploadXML)
         ]
     }
 }
