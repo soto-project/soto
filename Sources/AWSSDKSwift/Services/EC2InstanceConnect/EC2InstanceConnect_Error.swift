722ad5e41b535135ee695cb08ade87b65033ce46
Adam Fowler <adamfowler71@gmail.com>
2019-07-01 10:55:56 +0100

    Updates to new service files for Swift 5

--- Sources/AWSSDKSwift/Services/EC2InstanceConnect/EC2InstanceConnect_Error.swift
+++ Sources/AWSSDKSwift/Services/EC2InstanceConnect/EC2InstanceConnect_Error.swift
@@ -14,7 +14,7 @@ public enum EC2InstanceConnectErrorType: AWSErrorType {
 extension EC2InstanceConnectErrorType {
     public init?(errorCode: String, message: String?){
         var errorCode = errorCode
-        if let index = errorCode.index(of: "#") {
+        if let index = errorCode.firstIndex(of: "#") {
             errorCode = String(errorCode[errorCode.index(index, offsetBy: 1)...])
         }
         switch errorCode {
--- Sources/AWSSDKSwift/Services/Kafka/Kafka_Shapes.swift
+++ Sources/AWSSDKSwift/Services/Kafka/Kafka_Shapes.swift
@@ -476,8 +476,7 @@ extension Kafka {
         ///             The version of Apache Kafka.
         ///          
         public let kafkaVersion: String
-        ///             The number of broker nodes in the cluster.
-        ///          
+        /// The number of Kafka broker nodes in the Amazon MSK cluster.
         public let numberOfBrokerNodes: Int32
         ///             Create tags when creating the cluster.
         ///          
