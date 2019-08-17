b8092f1608191c36a3fdd9346ba49611c005368b
Adam Fowler <adamfowler71@gmail.com>
2019-08-17 23:38:28 +0100

    Update aws-sdk-swift-core version to 3.2

--- Package.swift
+++ Package.swift
@@ -193,7 +193,7 @@ let package = Package(
         .library(name: "XRay", targets: ["XRay"])
     ],
     dependencies: [
-        .package(url: "https://github.com/adam-fowler/aws-sdk-swift-core.git", .branch("validation"))
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "3.2.0"))
     ],
     targets: [
         .target(name: "ACM", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/AWSSDKSwift/Services/ACM"),
