0623ea64e3e137d7895cc988df9aba0215d39bba
Adam Fowler <adamfowler71@gmail.com>
2019-09-17 07:36:36 +0100

    Update to using v3.3.0 of aws-sdk-swift-core

--- Package.swift
+++ Package.swift
@@ -200,7 +200,7 @@ let package = Package(
         .library(name: "XRay", targets: ["XRay"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .branch("master"))
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "3.3.0"))
     ],
     targets: [
         .target(name: "ACM", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/AWSSDKSwift/Services/ACM"),
