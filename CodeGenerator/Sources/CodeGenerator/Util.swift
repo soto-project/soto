e4d4e66d7ec376ab95b399d8b0f45417286e802e
Andrea Scuderi <andreascuderi@ymail.com>
2019-09-26 21:01:07 +0100

    Add canImport(FoundationNetworking)

--- CodeGenerator/Sources/CodeGenerator/Util.swift
+++ CodeGenerator/Sources/CodeGenerator/Util.swift
@@ -7,6 +7,9 @@
 //
 
 import Foundation
+#if canImport(FoundationNetworking)
+    import FoundationNetworking
+#endif
 import SwiftyJSON
 
 let enableShowLog = ProcessInfo.processInfo.environment["Verbose"] == nil ? false : true
--- Sources/AWSSDKSwift/Middlewares/S3/S3RequestMiddleware.swift
+++ Sources/AWSSDKSwift/Middlewares/S3/S3RequestMiddleware.swift
@@ -1,4 +1,7 @@
 import Foundation
+#if canImport(FoundationNetworking)
+    import FoundationNetworking
+#endif
 import AWSSDKSwiftCore
 
 public struct S3RequestMiddleware: AWSServiceMiddleware {
