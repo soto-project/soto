f18bb25f25f3b8a4eb0e340fd9eadc9f2a69743c
Adam Fowler <adamfowler71@gmail.com>
2019-10-23 12:58:22 +0100

    Added IAMTests.testSetGetPolicy()
    Also moved tearDown() call to defer() to ensure it gets called

--- Tests/AWSSDKSwiftTests/Services/IAM/IAMTests.swift
+++ Tests/AWSSDKSwiftTests/Services/IAM/IAMTests.swift
@@ -38,9 +38,11 @@ class IAMTests: XCTestCase {
     }
     
     /// teardown test
-    func tearDown(_ testData: TestData) throws {
-        let request = IAM.DeleteUserRequest(userName: testData.userName)
-        try client.deleteUser(request).wait()
+    func tearDown(_ testData: TestData) {
+        attempt {
+            let request = IAM.DeleteUserRequest(userName: testData.userName)
+            try client.deleteUser(request).wait()
+        }
     }
     
     //MARK: TESTS
@@ -49,18 +51,60 @@ class IAMTests: XCTestCase {
         attempt {
             let testData = TestData(#function)
             try setup(testData)
-            
+            defer {
+                tearDown(testData)
+            }
+
             let request = IAM.GetUserRequest(userName: testData.userName)
             let response = try client.getUser(request).wait()
             XCTAssertEqual(response.user.userName, testData.userName)
-            
-            try tearDown(testData)
         }
     }
 
+    func testSetGetPolicy() {
+        attempt {
+            let testData = TestData(#function)
+            try setup(testData)
+            defer {
+                tearDown(testData)
+            }
+
+            // put a policy on the user
+            var policyDocument = """
+                        {
+                            "Version": "2012-10-17",
+                            "Statement": [
+                                {
+                                    "Effect": "Allow",
+                                    "Action": [
+                                        "sns:*",
+                                        "s3:*",
+                                        "sqs:*"
+                                    ],
+                                    "Resource": "*"
+                                }
+                            ]
+                        }
+                        """
+            policyDocument = policyDocument.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+            let getUserRequest = IAM.GetUserRequest(userName: testData.userName)
+            let getUserResponse = try client.getUser(getUserRequest).wait()
+            
+            let putRequest = IAM.PutUserPolicyRequest(policyDocument: policyDocument, policyName: "testSimulatePolicy", userName: getUserResponse.user.userName)
+            _ = try client.putUserPolicy(putRequest).wait()
+            
+            let getRequest = IAM.GetUserPolicyRequest(policyName: "testSimulatePolicy", userName: getUserResponse.user.userName)
+            let getResponse = try client.getUserPolicy(getRequest).wait()
+            
+            XCTAssertEqual(getResponse.policyDocument.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines), policyDocument)
+        }
+
+    }
+    
     static var allTests : [(String, (IAMTests) -> () throws -> Void)] {
         return [
             ("testCreateDeleteUser", testCreateDeleteUser),
+            ("testSetGetPolicy", testSetGetPolicy),
         ]
     }
 }
