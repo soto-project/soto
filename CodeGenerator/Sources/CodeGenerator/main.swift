1a70737fefbcbbba48349d34a425c0eeed8988a9
Adam Fowler <adamfowler71@gmail.com>
2020-01-23 11:11:12 +0000

    Added writeIfChanged to codegenerator
    Don't write file out if it hasn't changed. Should speed up the process of building after running the code generator as it won't rebuild files that havent changed

--- CodeGenerator/Sources/CodeGenerator/main.swift
+++ CodeGenerator/Sources/CodeGenerator/main.swift
@@ -4,6 +4,20 @@ import Dispatch
 import Stencil
 import PathKit
 
+extension String {
+    func writeIfChanged(toFile: String, atomically: Bool, encoding: String.Encoding) throws -> Bool {
+        do {
+            let original = try String(contentsOfFile: toFile)
+            guard original != self else { return false }
+        } catch {
+            print(error)
+        }
+        try write(toFile: toFile, atomically: atomically, encoding: encoding)
+        return true
+    }
+}
+
+
 let startTime = Date()
 let models = try loadModelJSON()
 /*let apis = try loadAPIJSONList()
@@ -30,45 +44,55 @@ for model in models {
             _ = mkdirp(basePath)
 
             let apiContext = service.generateServiceContext()
-            try environment.renderTemplate(name: "api.stencil", context: apiContext).write(
+            if try environment.renderTemplate(name: "api.stencil", context: apiContext).writeIfChanged(
                     toFile: "\(basePath)/\(service.serviceName)_API.swift",
                     atomically: true,
                     encoding: .utf8
-                )
+                ) {
+                print("Wrote: \(service.serviceName)_API.swift")
+            }
 
             let shapesContext = service.generateShapesContext()
-            try environment.renderTemplate(name: "shapes.stencil", context: shapesContext).write(
+            if try environment.renderTemplate(name: "shapes.stencil", context: shapesContext).writeIfChanged(
                 toFile: "\(basePath)/\(service.serviceName)_Shapes.swift",
                 atomically: true,
                 encoding: .utf8
-            )
+                ) {
+                print("Wrote: \(service.serviceName)_Shapes.swift")
+            }
 
             let errorContext = service.generateErrorContext()
             if errorContext["errors"] != nil {
-                try environment.renderTemplate(name: "error.stencil", context: errorContext).write(
+                if try environment.renderTemplate(name: "error.stencil", context: errorContext).writeIfChanged(
                     toFile: "\(basePath)/\(service.serviceName)_Error.swift",
                     atomically: true,
                     encoding: .utf8
-                )
+                    ) {
+                    print("Wrote: \(service.serviceName)_Error.swift")
+                }
             }
 
             let paginatorContext = service.generatePaginatorContext()
             if paginatorContext["paginators"] != nil {
-                try environment.renderTemplate(name: "paginator.stencil", context: paginatorContext).write(
+                if try environment.renderTemplate(name: "paginator.stencil", context: paginatorContext).writeIfChanged(
                     toFile: "\(basePath)/\(service.serviceName)_Paginator.swift",
                     atomically: true,
                     encoding: .utf8
-                )
+                    ) {
+                       print("Wrote: \(service.serviceName)_Paginator.swift")
+                }
             }
 
             let customTemplates = service.getCustomTemplates()
             for template in customTemplates {
                 let templateName = URL(fileURLWithPath: template).deletingPathExtension().lastPathComponent
-                try environment.renderTemplate(name: template).write(
+                if try environment.renderTemplate(name: template).writeIfChanged(
                     toFile: "\(basePath)/\(service.serviceName)+\(templateName).swift",
                     atomically: true,
                     encoding: .utf8
-                )
+                    ) {
+                    print("Wrote: \(service.serviceName)+\(templateName).swift")
+                }
             }
             print("Succesfully Generated \(service.serviceName)")
             group.leave()
@@ -82,3 +106,4 @@ for model in models {
 group.wait()
 print("Code Generation took \(Int(-startTime.timeIntervalSinceNow)) seconds")
 print("Done.")
+
