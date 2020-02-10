dc740f3c83be9438761d32b2d5e86cb7d0d00c45
Adam Fowler <adamfowler71@gmail.com>
2020-02-10 22:52:23 +0000

    SwiftNIO security updates
    Update aws-sdk-swift-core version to bring in latest security updates from Swift NIO
    https://github.com/apple/swift-nio/releases/tag/2.13.1
    https://github.com/apple/swift-nio-ssl/security/advisories/GHSA-9556-94c5-c3j8

--- Package.swift
+++ Package.swift
@@ -230,7 +230,7 @@ let package = Package(
         .library(name: "XRay", targets: ["XRay"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.2.0"))
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.2.2"))
     ],
     targets: [
         .target(name: "ACM", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/AWSSDKSwift/Services/ACM"),
