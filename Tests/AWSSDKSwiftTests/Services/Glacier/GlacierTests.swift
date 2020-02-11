54694ee8e4fe445935972b80292367be07d37789
Adam Fowler <adamfowler71@gmail.com>
2020-02-11 09:52:02 +0000

    AWSGlacier not AWSGlacierMiddleware

--- Tests/AWSSDKSwiftTests/Services/Glacier/GlacierTests.swift
+++ Tests/AWSSDKSwiftTests/Services/Glacier/GlacierTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 import Foundation
 import NIO
-@testable import AWSGlacierMiddleware
+@testable import AWSGlacier
 
 class GlacierTests: XCTestCase {
 
@@ -22,7 +22,7 @@ class GlacierTests: XCTestCase {
             w = 18000 * (w & 65535) + (w >> 16);
             return UInt8(((z<<16)+w) & 0xff)
         }
-        
+
         //  create buffer full of random data, use the same seeds to ensure we get the same buffer everytime
         let size = 7*1024*1024 + 258
         var data = Data(count: size)
@@ -32,11 +32,11 @@ class GlacierTests: XCTestCase {
         // create byte buffer
         var byteBuffer = ByteBufferAllocator().buffer(capacity: data.count)
         byteBuffer.writeBytes(data)
-        
+
         let middleware = GlacierRequestMiddleware(apiVersion: "2012-06-01")
         let treeHash = try middleware.computeTreeHash(byteBuffer)
-        
+
         XCTAssertEqual(treeHash, [210, 50, 5, 126, 16, 6, 59, 6, 21, 40, 186, 74, 192, 56, 39, 85, 210, 25, 238, 54, 4, 252, 221, 238, 107, 127, 76, 118, 245, 76, 22, 45])
     }
-    
+
 }
