6e83333556026df1284969e175db6c4786f37741
Adam Fowler <adamfowler71@gmail.com>
2020-03-20 15:17:57 +0000

    Use aws-sdk-swift-core master branch

--- Package.swift
+++ Package.swift
@@ -228,7 +228,7 @@ let package = Package(
         .library(name: "AWSXRay", targets: ["AWSXRay"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .branch("body-remove-asdata"))
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .branch("master"))
     ],
     targets: [
         .target(name: "AWSACM", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/AWSSDKSwift/Services/ACM"),
