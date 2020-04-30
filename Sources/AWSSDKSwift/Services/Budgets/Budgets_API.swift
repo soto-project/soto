21a915e06c17d249a0a9e0ff485d9999c306a8c0
Adam Fowler <adamfowler71@gmail.com>
2020-04-30 13:06:29 +0100

    Remove region parameter from services that don't require a region
    If it is left in the user can specify an invalid region

--- Sources/AWSSDKSwift/Services/Budgets/Budgets_API.swift
+++ Sources/AWSSDKSwift/Services/Budgets/Budgets_API.swift
@@ -36,7 +36,6 @@ public struct Budgets {
     ///     - accessKeyId: Public access key provided by AWS
     ///     - secretAccessKey: Private access key provided by AWS
     ///     - sessionToken: Token provided by STS.AssumeRole() which allows access to another AWS account
-    ///     - region: Region of server you want to communicate with
     ///     - endpoint: Custom endpoint URL to use instead of standard AWS servers
     ///     - middlewares: Array of middlewares to apply to requests and responses
     ///     - httpClientProvider: HTTPClient to use. Use `createNew` if the client should manage its own HTTPClient.
@@ -44,7 +43,6 @@ public struct Budgets {
         accessKeyId: String? = nil,
         secretAccessKey: String? = nil,
         sessionToken: String? = nil,
-        region: AWSSDKSwiftCore.Region? = nil,
         endpoint: String? = nil,
         middlewares: [AWSServiceMiddleware] = [],
         httpClientProvider: AWSClient.HTTPClientProvider = .createNew
@@ -53,7 +51,7 @@ public struct Budgets {
             accessKeyId: accessKeyId,
             secretAccessKey: secretAccessKey,
             sessionToken: sessionToken,
-            region: region,
+            region: nil,
             amzTarget: "AWSBudgetServiceGateway",
             service: "budgets",
             serviceProtocol: .json(version: "1.1"),
