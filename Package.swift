55ff8ad56096b1eda6f6fdcc7a2578094733dc29
Adam Fowler <adamfowler71@gmail.com>
2020-05-01 15:18:54 +0100

    Using v5.0.0-alpha.3.2 of aws-sdk-swift-core

--- Package.swift
+++ Package.swift
@@ -247,7 +247,7 @@ let package = Package(
         .library(name: "AWSXRay", targets: ["AWSXRay"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "5.0.0-alpha.2"))
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "5.0.0-alpha.3.2"))
     ],
     targets: [
         .target(name: "AWSACM", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/AWSSDKSwift/Services/ACM"),
