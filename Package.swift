486b6920d26e4904c60b2c2f70dcdf6e2b2295f0
Adam Fowler <adamfowler71@gmail.com>
2020-05-01 15:18:54 +0100

    Using v5.0.0-alpha.3.2 of aws-sdk-swift-core

--- Package.swift
+++ Package.swift
@@ -256,7 +256,7 @@ let package = Package(
         .library(name: "SotoXRay", targets: ["SotoXRay"])
     ],
     dependencies: [
-        .package(url: "https://github.com/soto-project/soto-core.git", .upToNextMinor(from: "5.0.0-alpha.2"))
+        .package(url: "https://github.com/soto-project/soto-core.git", .upToNextMinor(from: "5.0.0-alpha.3"))
     ],
     targets: [
         .target(name: "SotoACM", dependencies: ["SotoCore"], path: "./Sources/Soto/Services/ACM"),
