30d581558acc10b4132a3cccfba02783ac8ebb0c
Adam Fowler <adamfowler71@gmail.com>
2020-05-01 14:45:30 +0100

    Upped aws-sdk-swift-version to 4.5

--- Package.swift
+++ Package.swift
@@ -230,7 +230,7 @@ let package = Package(
         .library(name: "XRay", targets: ["XRay"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.4.0"))
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.5.0"))
     ],
     targets: [
         .target(name: "ACM", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/AWSSDKSwift/Services/ACM"),
