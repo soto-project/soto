c5102c823a27a2db9a4f3cee3ab96320328e5512
Adam Fowler <adamfowler71@gmail.com>
2020-05-27 15:00:09 +0100

    Commit documentation to aws-sdk-swift-docs

--- scripts/gen-doc.sh
+++ scripts/gen-doc.sh
@@ -37,31 +37,29 @@ tidy_up() {
     rm -rf docs/docsets
 }
 
-move_docs_to_gh_pages() {
-    # stash everything that isn't in docs, store result in STASH_RESULT
-    STASH_RESULT=$(git stash push -- ":(exclude)docs")
-    # get branch name
-    CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+FOLDER=3.x.x
+BRANCH=gh-pages
+
+move_docs_to_aws_sdk_swift_docs() {
     REVISION_HASH=$(git rev-parse HEAD)
     COMMIT_MSG="Documentation for https://github.com/swift-aws/aws-sdk-swift/tree/$REVISION_HASH"
     if [ -n "$1" ]; then
         COMMIT_MSG=$1
     fi
 
-    git checkout gh-pages
+    git clone https://github.com/swift-aws/aws-sdk-swift-docs -b "$BRANCH"
+    
+    cd aws-sdk-swift-docs
     # copy contents of docs to docs/current replacing the ones that are already there
-    rm -rf current
-    mv docs/ current/
+    rm -rf "$FOLDER"
+    mv ../docs/ "$FOLDER"/
     # commit
-    git add --all current
+    git add --all "$FOLDER"
     git commit -m "$COMMIT_MSG"
     git push
-    # return to branch
-    git checkout "$CURRENT_BRANCH"
-
-    if [ "$STASH_RESULT" != "No local changes to save" ]; then
-        git stash pop
-    fi
+    
+    cd ..
+    rm -rf aws-sdk-swift-docs
 }
 
 COMMIT_MSG=""
@@ -78,4 +76,4 @@ create_aws_sdk_swift_docs_json
 combine_docs_json
 run_jazzy
 tidy_up
-move_docs_to_gh_pages "$COMMIT_MSG"
+move_docs_to_aws_sdk_swift_docs "$COMMIT_MSG"
