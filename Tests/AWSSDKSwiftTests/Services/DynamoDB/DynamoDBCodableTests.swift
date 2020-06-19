7c2129ae4e5ae2359ff3942eaa65d11f963c7f2a
Adam Fowler <adamfowler71@gmail.com>
2020-06-19 10:48:48 +0100

    Add file headers

--- Tests/AWSSDKSwiftTests/Services/DynamoDB/DynamoDBCodableTests.swift
+++ Tests/AWSSDKSwiftTests/Services/DynamoDB/DynamoDBCodableTests.swift
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the AWSSDKSwift open source project
+//
+// Copyright (c) 2017-2020 the AWSSDKSwift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of AWSSDKSwift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import AWSDynamoDB
 import NIO
 import XCTest
