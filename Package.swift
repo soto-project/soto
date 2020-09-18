dbe43ab43455c7b2656874841580fb4aa8938184
Adam Fowler <adamfowler71@gmail.com>
2020-09-18 08:46:25 +0100

    Use soto-core release 5.0.0-beta.1

--- Package.swift
+++ Package.swift
@@ -279,7 +279,7 @@ let package = Package(
         .library(name: "SotoXRay", targets: ["SotoXRay"])
     ],
     dependencies: [
-        .package(url: "https://github.com/soto-project/soto-core.git", .upToNextMinor(from: "5.0.0-alpha.6"))
+        .package(url: "https://github.com/soto-project/soto-core.git", .upToNextMinor(from: "5.0.0-beta.1"))
     ],
     targets: [
         .target(name: "SotoACM", dependencies: [.product(name: "SotoCore", package: "soto-core")], path: "./Sources/Soto/Services/ACM"),
