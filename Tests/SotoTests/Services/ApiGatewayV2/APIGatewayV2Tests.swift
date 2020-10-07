3f199f0eccac2f812fe56002510efd1d3af2fcd3
Adam Fowler <adamfowler71@gmail.com>
2020-10-07 16:05:28 +0100

    Fix ApiGatewayV2.testGetApis occasional fail
    Set date to be a minute prior instead of a second to avoid server time inaccuracies

--- Tests/SotoTests/Services/ApiGatewayV2/APIGatewayV2Tests.swift
+++ Tests/SotoTests/Services/ApiGatewayV2/APIGatewayV2Tests.swift
@@ -88,7 +88,8 @@ class APIGatewayV2Tests: XCTestCase {
 
     /// tests whether created date is loading correctly
     func testGetApis() {
-        let date = Date(timeIntervalSinceNow: -1.0)
+        // get date from 1 minute before now.
+        let date = Date(timeIntervalSinceNow: -60.0)
         let response = self.testRestApi { id in
             return Self.apiGatewayV2.getApis(.init())
                 .flatMapThrowing { response in
