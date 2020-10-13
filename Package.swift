b96af15db8d9f4d2fc24355515d2dbaa6e88678a
Adam Fowler <adamfowler71@gmail.com>
2020-10-13 13:01:32 +0100

    Use main branch of soto-core

--- Package.swift
+++ Package.swift
@@ -256,7 +256,7 @@ let package = Package(
         .library(name: "SotoXRay", targets: ["SotoXRay"])
     ],
     dependencies: [
-        .package(url: "https://github.com/soto-project/soto-core.git", .branch("paginate-reduce"))
+        .package(url: "https://github.com/soto-project/soto-core.git", .branch("main"))
     ],
     targets: [
         .target(name: "SotoACM", dependencies: ["SotoCore"], path: "./Sources/Soto/Services/ACM"),
