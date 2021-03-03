e416abc906212f738bca81da419489bc716e49c9
Adam Fowler <adamfowler71@gmail.com>
2021-03-03 12:13:15 +0000

    Use main branch

--- Package.swift
+++ Package.swift
@@ -281,7 +281,7 @@ let package = Package(
         .library(name: "SotoXRay", targets: ["SotoXRay"])
     ],
     dependencies: [
-        .package(url: "https://github.com/soto-project/soto-core.git", .branch("paginate-input-token-test"))
+        .package(url: "https://github.com/soto-project/soto-core.git", .branch("main"))
     ],
     targets: [
         .target(name: "SotoACM", dependencies: [.product(name: "SotoCore", package: "soto-core")], path: "./Sources/Soto/Services/ACM"),
