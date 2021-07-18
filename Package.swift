fa2368c2e49a99f4fd0b994c5d6818cbf96edbbd
Adam Fowler <adamfowler71@gmail.com>
2021-07-18 07:35:45 +0100

    Set minimum requirement to be swift 5.2 (#505)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 //===----------------------------------------------------------------------===//
 //
 // This source file is part of the Soto for AWS open source project
