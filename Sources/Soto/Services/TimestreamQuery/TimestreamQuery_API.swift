06a0becdf8b8dc67d33fd0d68baced498c4751a8
Adam Fowler <adamfowler71@gmail.com>
2021-08-25 15:44:58 +0100

    Generate service files

--- Sources/Soto/Services/TimestreamQuery/TimestreamQuery_API.swift
+++ Sources/Soto/Services/TimestreamQuery/TimestreamQuery_API.swift
@@ -24,6 +24,8 @@ public struct TimestreamQuery: AWSService {
     public let client: AWSClient
     /// Service configuration
     public let config: AWSServiceConfig
+    /// endpoint storage
+    let endpointStorage: EndpointStorage
 
     // MARK: Initialization
 
@@ -58,13 +60,14 @@ public struct TimestreamQuery: AWSService {
             byteBufferAllocator: byteBufferAllocator,
             options: options
         )
+        self.endpointStorage = .init(endpoint: self.config.endpoint)
     }
 
     // MARK: API Calls
 
     ///  Cancels a query that has been issued. Cancellation is guaranteed only if the query has not completed execution before the cancellation request was issued. Because cancellation is an idempotent operation, subsequent cancellation requests will return a CancellationMessage, indicating that the query has already been canceled.
     public func cancelQuery(_ input: CancelQueryRequest, logger: Logger = AWSClient.loggingDisabled, on eventLoop: EventLoop? = nil) -> EventLoopFuture<CancelQueryResponse> {
-        return self.client.execute(operation: "CancelQuery", path: "/", httpMethod: .POST, serviceConfig: self.config, input: input, logger: logger, on: eventLoop)
+        return self.client.execute(operation: "CancelQuery", path: "/", httpMethod: .POST, serviceConfig: self.config, input: input, endpointDiscovery: .init(storage: self.endpointStorage, discover: self.getEndpoint, required: true), logger: logger, on: eventLoop)
     }
 
     /// DescribeEndpoints returns a list of available endpoints to make Timestream API calls against. This API is available through both Write and Query. Because Timestream’s SDKs are designed to transparently work with the service’s architecture, including the management and mapping of the service endpoints, it is not recommended that you use this API unless:   Your application uses a programming language that does not yet have SDK support   You require better control over the client-side implementation   For detailed information on how to use DescribeEndpoints, see The Endpoint Discovery Pattern and REST APIs.
@@ -74,7 +77,15 @@ public struct TimestreamQuery: AWSService {
 
     ///  Query is a synchronous operation that enables you to execute a query. Query will timeout after 60 seconds. You must update the default timeout in the SDK to support a timeout of 60 seconds. The result set will be truncated to 1MB. Service quotas apply. For more information, see Quotas in the Timestream Developer Guide.
     public func query(_ input: QueryRequest, logger: Logger = AWSClient.loggingDisabled, on eventLoop: EventLoop? = nil) -> EventLoopFuture<QueryResponse> {
-        return self.client.execute(operation: "Query", path: "/", httpMethod: .POST, serviceConfig: self.config, input: input, logger: logger, on: eventLoop)
+        return self.client.execute(operation: "Query", path: "/", httpMethod: .POST, serviceConfig: self.config, input: input, endpointDiscovery: .init(storage: self.endpointStorage, discover: self.getEndpoint, required: true), logger: logger, on: eventLoop)
+    }
+
+    func getEndpoint(logger: Logger, eventLoop: EventLoop) -> EventLoopFuture<AWSEndpoints> {
+        return self.describeEndpoints(.init(), logger: logger, on: eventLoop).map {
+            .init(endpoints: $0.endpoints.map {
+                .init(address: "https://\($0.address)", cachePeriodInMinutes: $0.cachePeriodInMinutes)
+            })
+        }
     }
 }
 
@@ -84,5 +95,6 @@ extension TimestreamQuery {
     public init(from: TimestreamQuery, patch: AWSServiceConfig.Patch) {
         self.client = from.client
         self.config = from.config.with(patch: patch)
+        self.endpointStorage = .init(endpoint: self.config.endpoint)
     }
 }
