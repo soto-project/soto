28ee08df78bff9a5f5d5f05a00b2d6845c4792a7
Adam Fowler <adamfowler71@gmail.com>
2021-12-17 17:38:55 +0000

    Use v6.5.0 version of SotoCore

--- Package.swift
+++ Package.swift
@@ -369,7 +369,7 @@ let package = Package(
         .library(name: "SotoXRay", targets: ["SotoXRay"])
     ],
     dependencies: [
-        .package(url: "https://github.com/soto-project/soto-core.git", branch: "main")
+        .package(url: "https://github.com/soto-project/soto-core.git", from: "6.5.0")
     ],
     targets: [
         .target(name: "SotoACM", dependencies: [.product(name: "SotoCore", package: "soto-core")], path: "./Sources/Soto/Services/ACM"),
