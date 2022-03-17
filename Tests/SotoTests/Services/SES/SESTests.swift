467da39232eeb1093072e3274d8ca7ff2d964b39
Adam Fowler <adamfowler71@gmail.com>
2022-03-17 11:26:43 +0000

    Add SESTests.testGetAccountSendingEnabled (#581)
    * Add testGetAccountSendingEnabled
    
    * swift format
    
    * Skip testGetAccountSendingEnabled when running with Localstack

--- Tests/SotoTests/Services/SES/SESTests.swift
+++ Tests/SotoTests/Services/SES/SESTests.swift
@@ -40,6 +40,12 @@ class SESTests: XCTestCase {
         XCTAssertNoThrow(try Self.client.syncShutdown())
     }
 
+    // Tests query protocol requests with no body
+    func testGetAccountSendingEnabled() throws {
+        try XCTSkipIf(TestEnvironment.isUsingLocalstack)
+        XCTAssertNoThrow(_ = try Self.ses.getAccountSendingEnabled().wait())
+    }
+
     /* func testSESIdentityExistsWaiter() {
          let response = Self.ses.verifyEmailIdentity(.init(emailAddress: "admin@opticalaberration.com"))
              .flatMap{ _ in
