d0eec3e1d2f3ca89742357a2f4c9b6ec43eb4000
Adam Fowler <adamfowler71@gmail.com>
2023-10-24 11:35:39 +0100

    Add @_spi(SotoInternal)

--- Package.swift
+++ Package.swift
@@ -373,7 +373,7 @@ let package = Package(
         .library(name: "SotoXRay", targets: ["SotoXRay"])
     ],
     dependencies: [
-        .package(url: "https://github.com/soto-project/soto-core.git", from: "7.0.0-alpha.1")
+        .package(url: "https://github.com/soto-project/soto-core.git", branch: "7.x.x")
     ],
     targets: [
         .target(name: "SotoACM", dependencies: [.product(name: "SotoCore", package: "soto-core")], path: "./Sources/Soto/Services/ACM"),
