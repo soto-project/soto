3ed40d508f8a99caf80e28bb1c4f837cc2b21912
Nick Sloan <all+github@nicksloan.com>
2024-03-29 19:45:35 +0000

    More work on implementation

--- Sources/Soto/Extensions/S3/S3+presignedPost.swift
+++ Sources/Soto/Extensions/S3/S3+presignedPost.swift
@@ -43,31 +43,35 @@ extension S3 {
 
     /// 
     @Sendable
-    public func generatePresignedPost(key: String, bucket: String, fields: [String: String] = [:], conditions: [PostPolicyCondition] = []) -> PresignedPostResponse {
+    public func generatePresignedPost(key: String, bucket: String, fields: [String: String] = [:], conditions: [PostPolicyCondition] = [], expiresIn: TimeInterval) -> PresignedPostResponse {
         // Copy the fields and conditions to a variable
         var fields = fields
         var conditions = conditions
 
+        // Update endpoint URL to include the bucket
+        var url = URL(from: self.config.endpoint)
+        url.host = [bucket, url.host].joined(separator: ".")
+
         // Gather canonical values
-        let url = URL(from: self.config.endpoint)
         let algorithm = "AWS4-HMAC-SHA256" // Get signature version from client?
         let date = Date.now
+        let region = self.config.region
 
         // Add required conditions
         conditions.append(.match("bucket", bucket))
         conditions.append(.match("key", key))
         conditions.append(.match("x-amz-algorithm", algorithm))
         conditions.append(.match("x-amz-date", "")) // TODO
-        conditions.append(.match("x-amz-credential", "")) // TODO
+        conditions.append(.match("x-amz-credential", getCredential()))
 
         // Add required fields
         fields["key"] = key
         fields["x-amz-algorithm"] = algorithm
         fields["x-amz-date"] = "" // TODO
-        fields["x-amz-credential"] = "" // TODO
+        fields["x-amz-credential"] = getCredential()
 
         // Create the policy and add to fields
-        let policy = PostPolicy(expiration: Date(), conditions: conditions)
+        let policy = PostPolicy(expiration: date.adding(expiresIn), conditions: conditions)
         let stringToSign = policy.stringToSign()
         fields["Policy"] = stringToSign
 
@@ -81,16 +85,34 @@ extension S3 {
         return presignedPostResponse
     }
 
-    private func getSigningKey() -> SymmetricKey {
+    private func signingKey(date: String) -> SymmetricKey {
+        let credentials = await client.credentialProvider.getCredential()
+        let name = client.config.name
+        let region = client.config.region
 
+        let kDate = HMAC<SHA256>.authenticationCode(for: [UInt8](date.utf8), using: SymmetricKey(data: Array("AWS4\(credentials.secretAccessKey)".utf8)))
+        let kRegion = HMAC<SHA256>.authenticationCode(for: [UInt8](region.utf8), using: SymmetricKey(data: kDate))
+        let kService = HMAC<SHA256>.authenticationCode(for: [UInt8](name.utf8), using: SymmetricKey(data: kRegion))
+        let kSigning = HMAC<SHA256>.authenticationCode(for: [UInt8]("aws4_request".utf8), using: SymmetricKey(data: kService))
+        return SymmetricKey(data: kSigning)
     }
 
-    private func getSignature(policy: String) -> String {
-        return ""
+    private func getSignature(policy: String, date: String) -> String {
+        let key = signingKey(date: date)
+        let signature = HMAC<SHA256>.authenticationCode(for: Data(stringToSign.utf8), using: key)
+        return signature
     }
 
     private func getCredential() -> String {
-        return ""
+        let credentials = await client.credentialProvider.getCredential()
+
+        let accessKeyID = credentials.accessKeyID
+        let date = date
+        let region = config.region
+        let service = config.name
+
+        let credential = "\(accessKeyID)/\(date)/\(region)/\(service)"
+        return credential
     }
 
 }
