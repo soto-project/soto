cf51d2e4ef71971b705aec1201908a368259937c
Nick Sloan <all+github@nicksloan.com>
2024-04-01 16:51:15 +0000

    Add license

--- Sources/Soto/Extensions/S3/S3+presignedPost.swift
+++ Sources/Soto/Extensions/S3/S3+presignedPost.swift
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Soto for AWS open source project
+//
+// Copyright (c) 2017-2021 the Soto project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Soto project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 import Logging
 import Foundation
