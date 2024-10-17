74b99527a8e8176724ae17deeb12b88f27da25f9
Adam Fowler <adamfowler71@gmail.com>
2024-10-17 09:39:27 +0100

    Export generated service files from extensions target (#738)
    * export Service files from extensions
    
    * Append Generated to generated service targer

---
+++ Sources/Soto/Extensions/CognitoIdentity/exports.swift
@@ -0,0 +1,15 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Soto for AWS open source project
+//
+// Copyright (c) 2024 the Soto project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Soto project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+@_exported import _SotoCognitoIdentityGenerated
