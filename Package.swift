7eb88f2558101e6a242547b365cc17cdc05c932e
Adam Fowler <adamfowler71@gmail.com>
2025-05-29 09:56:45 +0100

    Use soto-core 7.7.1

--- Package.swift
+++ Package.swift
@@ -433,7 +433,7 @@ let package = Package(
         .library(name: "SotoXRay", targets: ["SotoXRay"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/soto-project/soto-core.git", from: "7.6.0")
+        .package(url: "https://github.com/soto-project/soto-core.git", from: "7.7.1")
     ],
     targets: [
         .target(
